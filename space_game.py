19b784e81777d803fe4abe754dbb8561e1e1d494
Jay Bhakhar <jaybhakhar@gmail.com>
2020-05-12 00:22:23 -0700

    11-5-2020 second commit

--- space_game.py
+++ space_game.py
@@ -1,10 +1,11 @@
 def space_game(x):
     x = str(x)
     co = x.count(" ")
-    if co//2 == 0:
+    if co%2 == 0:
         print("you won")
     else:
         print("you lose")
 
-space_game (" I'm going with a sword judge ")
+space_game ("I'm going with a sword judge")
 space_game("Hello")
+space_game('я иду с мечем судия')
